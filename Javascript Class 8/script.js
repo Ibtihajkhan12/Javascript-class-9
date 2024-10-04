c789a9d11edcbefff8f4c99d34d08cd2398e7cfe
Ibtihaj Khan <Ibtihaajkhaan@gmail.com>
2024-10-04 20:10:15 +0500

    Add files via upload

---
+++ Javascript Class 8/script.js
@@ -0,0 +1,29 @@
+
+//     const sentence = prompt("Please enter a sentence:");
+    
+//     // Convert the sentence to lowercase
+//     const lowerCaseSentence = sentence.toLowerCase()
+    
+//     // Initialize the vowel count
+//     let vowelCount = 0;
+
+//     // Loop through each character in the sentence
+//     for (let i = 0; i < lowerCaseSentence.length; i++) {
+//         // Check if the character is a vowel
+//         if (
+//             lowerCaseSentence[i] === 'a' ||
+//             lowerCaseSentence[i] === 'e' ||
+//             lowerCaseSentence[i] === 'i' ||
+//             lowerCaseSentence[i] === 'o' ||
+//             lowerCaseSentence[i] === 'u'
+//         ) {
+//             vowelCount++; // Increment the count for each vowel found
+//         }
+//     }
+
+//     // Display the total number of vowels
+//     alert(`Total number of vowels: ${vowelCount}`);
+// }
+
+// // Call the function to execute
+// countVowels();
